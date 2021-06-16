31a6f55cb730a754fd7611ea11fb6432e46a27f9
GharamAlessa <>
2021-06-16 16:05:24 +0000

    updated

--- app.js
+++ app.js
@@ -2,7 +2,4 @@ alert ("welcom to VOCO HOTEL")
 
 var answer=prompt("are you on vacation?.......please answer with yes or no")
 
-if (answer=="yes"){document.write("<p>"+"<h3>"+"Chasing sunsets? Take an evening walk along the river Thames in Oxford"+"</h3>"+"img src='https://digital.ihg.com/is/image/ihg/voco-oxford-6299119130-4x31'>"+"</div>")}
-
-
-else if (answer=="no"){document.write("<p>"+"<h3>"+"Meet the modern and beautifully designed Le Cafe at voco Reading"+"</h3>"+"<img src='https://www.ihgplc.com/-/media/voco-paris-montparnasse_restaurant.jpg?h=1024&la=en&w=1280&hash=63D7E30FB83CBB8130F0D17936B3CBDB'"+"</div>")}
+if (answer=="yes"){document.write("<div>"+"<h3>"+"Chasing sunsets? Take an evening walk along the river Thames in Oxford"+"</h3>"+"<img src='https://digital.ihg.com/is/image/ihg/voco-oxford-6299119130-4x3'/>"+"</div>")}else if (answer=="no"){document.write("<div>"+"<h3>"+"Meet the modern and beautifully designed Le Cafe at voco Reading"+"</h3>"+"<img src='https://www.ihgplc.com/-/media/voco-paris-montparnasse_restaurant.jpg?h=1024&la=en&w=1280&hash=63D7E30FB83CBB8130F0D17936B3CBDB'"+"</div>")}
