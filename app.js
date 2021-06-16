63f4af26a44e031aa704f25cc3a9a0e23b91d251
GharamAlessa <>
2021-06-16 15:16:51 +0000

    added app.js file

---
+++ app.js
@@ -0,0 +1,8 @@
+alert ("welcom to VOCO HOTEL")
+
+var answer=prompt("are you on vacation?.......please answer with yes or no")
+
+if (answer=="yes"){document.write("<p>"+"<h3>"+"Chasing sunsets? Take an evening walk along the river Thames in Oxford"+"</h3>"+"img src='https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/d0/a8/47/voco-oxford-thames.jpg?w=900&h=-1&s=1'>"+"</div>")}
+
+
+else if (answer=="no"){document.write("<p>"+"<h3>"+"Meet the modern and beautifully designed Le Cafe at voco Reading"+"</h3>"+"<img src='https://www.ihgplc.com/-/media/voco-paris-montparnasse_restaurant.jpg?h=1024&la=en&w=1280&hash=63D7E30FB83CBB8130F0D17936B3CBDB'"+"</div>")}
